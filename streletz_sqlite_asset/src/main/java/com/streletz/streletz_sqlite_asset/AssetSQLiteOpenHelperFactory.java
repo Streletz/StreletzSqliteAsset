ded1fdfc66f1d94db0a9c7615542f13b30f047ea
Streletz <streletzcoder@yandex.ru>
2019-08-17 13:13:27 +0300

    Перенос функционала и исправление ошибок.

---
+++ streletz_sqlite_asset/src/main/java/com/streletz/streletz_sqlite_asset/AssetSQLiteOpenHelperFactory.java
@@ -0,0 +1,29 @@
+package com.streletz.streletz_sqlite_asset;
+
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Implements {@link SupportSQLiteOpenHelper.Factory} using the SQLite implementation in the
+ * framework.
+ */
+@SuppressWarnings("unused")
+public class AssetSQLiteOpenHelperFactory implements SupportSQLiteOpenHelper.Factory {
+    @Override
+    public SupportSQLiteOpenHelper create(SupportSQLiteOpenHelper.Configuration configuration) {
+        return new AssetSQLiteOpenHelper(
+                configuration.context, configuration.name, null,
+                configuration.callback.version, dbObj -> {
+
+                }, configuration.callback
+        );
+    }
+
+    public SupportSQLiteOpenHelper create(SupportSQLiteOpenHelper.Configuration configuration, int version, DatabaseErrorHandler errorHandler) {
+        return new AssetSQLiteOpenHelper(
+                configuration.context, configuration.name, null,
+                version, errorHandler, configuration.callback
+        );
+    }
+}
